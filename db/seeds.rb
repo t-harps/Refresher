e034f5bea4c54d56b8141d3d9a15ff3c015d65d3
t-harps <t.w.harps@gmail.com>
2015-12-07 13:17:53 +0000

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 video1 = Video.create! :name => 'SlopeStyle One', :rank => 2, :link => 'https://vimeo.com/60000409', 
 											 :skier_list => 'Liam Downey, Maks Gorham, Will Berman, Pat Goodnough',
 											 :tag_list => 'park, breckenridge, medium'
-video2 = Video.create! :name => 'Leo T Shreds Las Leñas', :rank => 3, :link => 'https://www.youtube.com/watch?v=LdrKRVXCWPM', 
+video2 = Video.create! :name => 'Leo T Shreds Las Leñas', :rank => 4, :link => 'https://www.youtube.com/watch?v=LdrKRVXCWPM', 
 											 :skier_list => 'Leo Taillefer',
 											 :tag_list => 'big mountain, argentina, medium'																					 
 video3 = Video.create! :name => 'Kim Boberg Vs Niklas Larsson Zakrisson', :rank => 1, :link => 'http://www.slvsh.com/posts/229', 
 											 :skier_list => 'Kim Boberg, Niklas Larsson Zakrisson',
-											 :tag_list => 'slvsh, park, sweden, long'											 
+											 :tag_list => 'slvsh, park, sweden, long'
+video4 = Video.create! :name => 'Luka Melloni Vs Noah Albaladejo', :rank => 3, :link => 'http://www.slvsh.com/posts/42', 
+											 :skier_list => 'Luka Melloni, Noah Albaladejo',
+											 :tag_list => 'slvsh, park, Andorra, long'
+video5 = Video.create! :name => 'One of those days', :rank => 5, :link => 'https://www.youtube.com/watch?v=ygxufRprWpY', 
+											 :skier_list => 'Candide Thovex',
+											 :tag_list => 'resort, france, medium'																					 
+video6 = Video.create! :name => 'Reasons Japan Segment', :rank => 6, :link => 'https://www.youtube.com/watch?v=3exKo6RZc98', 
+											 :skier_list => 'JP Auclair, Chris Benchetler',
+											 :tag_list => 'powder, japan, long'											 											 
